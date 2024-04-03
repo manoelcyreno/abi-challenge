5e01f2dd0f20fa8c4055cb757f69533591ecbbc2
Manoel Cyreno <mf.misaki@gmail.com>
2024-04-03 19:47:54 +0200

    Add tests for the third point

---
+++ src/test/functional/tests/ReadUser.spec.js
@@ -0,0 +1,62 @@
+// @ts-nocheck
+const { test, expect } = require('@playwright/test');
+const { CommonMethods } = require('../utils/CommonMethods');
+
+test.describe('Test cases about: Get user and Validate information', () => {
+
+    const commonMethods = new CommonMethods();
+    const apiUrl = process.env.ABI_API_USER;
+    let userId = "TBD";
+    let body = "TBD";
+    let randomFirstName = "TBD";
+    let randomLastName = "TBD";
+    let randomUniqueId = "TBD";
+
+    test.beforeEach('Common Test Setup', async ({ page }) => {
+        // create user
+        randomFirstName = await commonMethods.generateRandomString();
+        randomLastName = await commonMethods.generateRandomString();
+        randomUniqueId = await commonMethods.generateRandomString();
+
+        body = {
+            age: 35,
+            dateOfBirth: '1988-08-21',
+            firstName: randomFirstName,
+            lastName: randomLastName,
+            gender: 'male',
+            language: 'es',
+            country: 'es',
+            location: 'es',
+            origin: 'br',
+            physicianCountry: 'es',
+            privacyAt: true,
+            subscriptionStartDate: '2021-11-17',
+            subscriptionEndDate: '2022-05-31',
+            uniqueId: randomUniqueId,
+            termsAt: true,
+            productName: 'Free trial'
+        };
+
+        const response = await commonMethods.makePostRequest(apiUrl, body);
+
+        expect(response).toHaveProperty('userStatus', 'processing');
+
+        userId = response.id;
+    });
+
+
+    test('Validate user information', async ({ page }) => {
+
+        const updateUserURL = apiUrl + "/" + userId;
+
+        const response = await commonMethods.makeGetRequest(updateUserURL);
+
+        expect(response).toHaveProperty('firstName', randomFirstName);
+        expect(response).toHaveProperty('origin', 'br');
+        expect(response).toHaveProperty('gender', 'male');
+        expect(response).toHaveProperty('location', 'es');
+        expect(response).toHaveProperty('uniqueId', randomUniqueId);
+
+    });
+
+});
--- src/test/functional/utils/CommonMethods.js
+++ src/test/functional/utils/CommonMethods.js
@@ -1,40 +1,53 @@
 class CommonMethods {
 
-  async generateRandomString() {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let randomString = '';
-    for (let i = 0; i < 6; i++) {
-        randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+    async generateRandomString() {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let randomString = '';
+
+        for (let i = 0; i < 6; i++) {
+            randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        
+        return randomString;
     }
-    return randomString;
 
-  }
+    async makePostRequest(url, postBody) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': '0797c976ee133afe6e4f6eb124da3ca3'
+            },
+            body: JSON.stringify(postBody)
+        });
 
-  async makePostRequest(url, postBody) {
-      const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'x-api-key': '0797c976ee133afe6e4f6eb124da3ca3'
-          },
-          body: JSON.stringify(postBody)
-      });
+        return response.json();
+    }
 
-      return response.json();
-  }
+    async makePutRequest(url, putBody) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': '0797c976ee133afe6e4f6eb124da3ca3'
+            },
+            body: JSON.stringify(putBody)
+        });
 
-  async makePutRequest(url, putBody) {
-    const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': '0797c976ee133afe6e4f6eb124da3ca3'
-        },
-        body: JSON.stringify(putBody)
-    });
+        return response.json();
+    }
 
-    return response.json();
-}
+    async makeGetRequest(url) {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': '0797c976ee133afe6e4f6eb124da3ca3'
+            }
+        });
+
+        return response.json();
+    }
 
 }
 
